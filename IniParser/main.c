5d03a22a8ebe04d883f0541ff2f2b577ed1d6e2b
a1987zz <a1987zz@mail.ru>
2021-02-10 12:26:18 +0300

    Add files via upload

---
+++ IniParser/main.c
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Common.h"
+#include "IniParser.h"
+
+
+int main()
+{
+    CIniParser* IniParser = new CIniParser();
+    IniParser->szFile = ExePath() + "\\Config.ini";
+
+    if (!FileExists(IniParser->szFile))
+    {
+        IniParser->WriteInt(std::to_string(7).c_str(), "bind", 32);
+    }
+
+
+    int key = IniParser->ReadInt(std::to_string(7).c_str(), "bind");
+    return 0;
+}
