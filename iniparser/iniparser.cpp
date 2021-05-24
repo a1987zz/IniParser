0e2ce31aa6f054b3f1fdb73ff49178765e0f5878
a1987zz <a1987zz@mail.ru>
2021-05-24 07:48:25 +0300

    Add files via upload

---
+++ iniparser/iniparser.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Common.h"
+#include "IniParser.h"
+
+using namespace std;
+int main()
+{
+    int key;
+    CIniParser* IniParser = new CIniParser();
+    IniParser->szFile = ExePath() + "\\Config.ini";
+
+    if (!FileExists(IniParser->szFile))
+    {
+        IniParser->WriteInt(std::to_string(7).c_str(), "bind", 32);
+        IniParser->Write(std::to_string(7).c_str(), "login", "123abc");
+        IniParser->Write(std::to_string(7).c_str(), "password", "abc123abc");
+    }
+    key = IniParser->ReadInt(std::to_string(7).c_str(), "bind");
+    char* keyss = _strdup(IniParser->ReadString(std::to_string(7).c_str(), "login").c_str());
+    char* keye = _strdup(IniParser->ReadString(std::to_string(7).c_str(), "password").c_str());
+
+    std::cout << key << std::endl;
+    std::cout << keyss << std::endl;
+    std::cout << keye << std::endl;
+    return 0;
+}
